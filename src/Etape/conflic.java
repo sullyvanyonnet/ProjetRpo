ec606d8dbef6d6cbcaf1ffff2a767f184a0ecaf6
yonnetsu <yonnetsu@localhost>
2018-12-03 10:59:06 +0100

    qsd

--- src/Etape/conflic.java
+++ src/Etape/conflic.java
@@ -10,15 +10,7 @@ public class conflic {
 		
 		//qsdqsd 
 		
-<<<<<<< HEAD
-		// bcp de comme 
-		//patate
-=======
-		// bcp de comme /// 4 
-		
-		//coucou
-		
->>>>>>> branch 'corentingrallsv-v0.1' of https://github.com/sullyvanyonnet/ProjetRpo.git
+
 	}
 	
 }
