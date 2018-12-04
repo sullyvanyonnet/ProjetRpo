a1b2e25ae34835b04d46740a8281adc41e1a018a
yonnetsu <yonnetsu@localhost>
2018-12-04 11:57:56 +0100

    Terrain

---
+++ src/modele/Terrain.java
@@ -0,0 +1,32 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Terrain implements Observateur{
+
+	ArrayList<ObjetGraphique> lesObjetGraphiques = new ArrayList<ObjetGraphique>();
+
+	Terrain(){
+		Fourmiliere uneFourmiliere = new Fourmiliere();
+		this.lesObjetGraphiques.add(uneFourmiliere);
+		uneFourmiliere.ajouterFourmi();
+		
+		
+	}
+	
+	
+	
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+		for (int i=0;i<this.lesObjetGraphiques.size();i++) {
+			this.lesObjetGraphiques.get(i).update();
+		}
+	}
+	
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
