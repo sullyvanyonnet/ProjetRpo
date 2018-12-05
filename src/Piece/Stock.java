2e9519dfde83f0d609f0dde5d11f121276dfabd2
yonnetsu <yonnetsu@localhost>
2018-12-05 11:17:58 +0100

    Stock

--- src/Piece/Stock.java
+++ src/Piece/Stock.java
@@ -1,6 +1,18 @@
 package Piece;
 
 public class Stock extends Salle {
-	int poidStocke;
+	int poidStocke = 0;
 	
+	void Ajouter(int unPoids){
+		this.poidStocke = this.poidStocke + unPoids;
+	}
+	
+	boolean suppr(int unPoids) {
+		
+		if(this.poidStocke <= unPoids) {
+			this.poidStocke = this.poidStocke - unPoids;
+			return true;
+		}
+		return false;	
+	}
 }
