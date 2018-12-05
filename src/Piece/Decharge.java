5846998f5c276006b7de157cb19287bd2a2a4e59
yonnetsu <yonnetsu@localhost>
2018-12-05 11:22:35 +0100

    Decharge avec ajout et suppr

--- src/Piece/Decharge.java
+++ src/Piece/Decharge.java
@@ -1,5 +1,25 @@
 package Piece;
 
+import java.util.ArrayList;
+
+import Fourmi.Fourmi;
+
 public class Decharge extends Salle{
 
+	int decharge = 0 ;
+	
+	ArrayList<Fourmi> listeFourmit = new ArrayList<Fourmi>();
+	
+	void Ajouter(int unPoids){
+		this.decharge = this.decharge + unPoids;
+	}
+	
+	void AjouterFourmi(Fourmi uneFourmi){
+		this.listeFourmit.add(uneFourmi);
+	}
+	
+	
+	void Suppr(Fourmi uneFourmi){
+		this.listeFourmit.remove(uneFourmi);
+	}
 }
