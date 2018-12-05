903472071849f2ead31a5f8346432413b3ce249b
rannouan <rannouan@localhost>
2018-12-05 15:54:24 +0100

    Fourmi_

--- src/Piece/Decharge.java
+++ src/Piece/Decharge.java
@@ -8,18 +8,18 @@ public class Decharge extends Salle{
 
 	int decharge = 0 ;
 	
-	ArrayList<Fourmi> listeFourmit = new ArrayList<Fourmi>();
+	ArrayList<Fourmi> listeFourmi = new ArrayList<Fourmi>();
 	
 	void Ajouter(int unPoids){
 		this.decharge = this.decharge + unPoids;
 	}
 	
 	void AjouterFourmi(Fourmi uneFourmi){
-		this.listeFourmit.add(uneFourmi);
+		this.listeFourmi.add(uneFourmi);
 	}
 	
 	
 	void Suppr(Fourmi uneFourmi){
-		this.listeFourmit.remove(uneFourmi);
+		this.listeFourmi.remove(uneFourmi);
 	}
 }
