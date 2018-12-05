282a8d0120c16587ec5fe5d4c78c53edc31a2394
rannouan <rannouan@localhost>
2018-12-05 15:57:53 +0100

    La mort c'est pas très joyeux

--- src/Piece/Decharge.java
+++ src/Piece/Decharge.java
@@ -2,24 +2,25 @@ package Piece;
 
 import java.util.ArrayList;
 
+import Etape.Cadavre;
 import Fourmi.Fourmi;
 
 public class Decharge extends Salle{
 
-	int decharge = 0 ;
+	int poidsTotal = 0 ;
 	
-	ArrayList<Fourmi> listeFourmi = new ArrayList<Fourmi>();
+	ArrayList<Cadavre> listeCadavre = new ArrayList<Cadavre>();
 	
 	void Ajouter(int unPoids){
-		this.decharge = this.decharge + unPoids;
+		this.poidsTotal = this.poidsTotal + unPoids;
 	}
 	
-	void AjouterFourmi(Fourmi uneFourmi){
-		this.listeFourmi.add(uneFourmi);
+	void AjouterFourmi(Cadavre uneFourmiMorte){
+		this.listeCadavre.add(uneFourmiMorte);
 	}
 	
 	
-	void Suppr(Fourmi uneFourmi){
-		this.listeFourmi.remove(uneFourmi);
+	void Suppr(Fourmi uneFourmiMorte){
+		this.listeCadavre.remove(uneFourmiMorte);
 	}
 }
