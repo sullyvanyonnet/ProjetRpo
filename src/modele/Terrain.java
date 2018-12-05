dd6db62674d706209959c94db3155203accba667
coren <coren@LAPTOP-5510MAPL>
2018-12-05 09:43:01 +0100

    terrain : ajout d'un arbre

--- src/modele/Terrain.java
+++ src/modele/Terrain.java
@@ -1,19 +1,33 @@
 package modele;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import vue.IMovableDrawable;
 import vue.Oval;
 import vue.World;
 
 public class Terrain implements Observateur{
 
+	private JPanel fenetrePrincipale;
 	private World leJardin;
+	private JTree tree;
 	private ArrayList<Fourmiliere> lesFourmilieres = new ArrayList<Fourmiliere>();
 
+
 	public Terrain(){
+		this.fenetrePrincipale = new JPanel(new BorderLayout());
+		  
 		this.leJardin = new World("Le Jardin");
 		this.leJardin.setBackground(Color.WHITE);
 		this.leJardin.setPreferredSize(new Dimension(800, 800));
@@ -25,7 +39,17 @@ public class Terrain implements Observateur{
 		int yFourmiliere = uneFourmiliere.getCoordonnees().getCoordonneeY();
 		this.leJardin.contents().put(-1, new Oval(Color.BLACK, new Point(xFourmiliere, yFourmiliere), new Dimension(20, 20)));
 		
+		for(int i = 0 ; i < this.lesFourmilieres.get(0).listeDeFourmis.size() ; i++ ) {
+			this.lesFourmilieres.get(0).listeDeFourmis.get(i).setCoordonnees(new Coordonnees(xFourmiliere, yFourmiliere));
+		}
 		
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode("The Java Series");
+		this.tree = new JTree(top);
+		
+		
+		this.fenetrePrincipale.add(this.leJardin);
+		this.fenetrePrincipale.add(this.tree);
+		this.fenetrePrincipale.show();
 	}
 
 	public ArrayList<Fourmiliere> getLesFourmilieres() {
@@ -46,13 +70,36 @@ public class Terrain implements Observateur{
 
 	@Override
 	public void updateH() {
-		// TODO Auto-generated method stub
 		
+		// TODO Auto-generated method stub
+		HashMap<Integer, IMovableDrawable> drawables = this.getLeJardin().contents();
+		for (int i = 0; i < this.lesFourmilieres.size(); i++) {
+			lesFourmilieres.get(i).updateH();
+		}
+
+		for (int i = 0; i < this.lesFourmilieres.size(); i++) {
+			
+			for (int x = 0; x < this.lesFourmilieres.get(i).listeDeFourmis.size(); x++) {
+				if(lesFourmilieres.get(i).listeDeFourmis.get(x).isPhase().equals("adulte")) {
+					int coordonneeX = lesFourmilieres.get(i).listeDeFourmis.get(x).getCoordonnees().getCoordonneeX();
+					int coordonneeY = lesFourmilieres.get(i).listeDeFourmis.get(x).getCoordonnees().getCoordonneeY();
+					IMovableDrawable unObjet = drawables.get(i);
+					unObjet.setPosition(new Point(coordonneeX, coordonneeY));
+				}
+
+			}
+		}
+		this.getLeJardin().repaint();		
 	}
 
 	@Override
 	public void updateJ() {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < this.lesFourmilieres.size(); i++) {
+			lesFourmilieres.get(i).updateJ();
+		}
+
+		this.getLeJardin().repaint();	
 	}
 
 }
