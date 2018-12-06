1c99f37b71db0fc473a80762812f372f6de27a2d
yonnetsu <yonnetsu@localhost>
2018-12-06 17:01:40 +0100

    test se deplacer OK allez retour . Ouvrier

---
+++ src/Role/TestOuvrier.java
@@ -0,0 +1,35 @@
+package Role;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+import Fourmi.Fourmi;
+
+public class TestOuvrier {
+	
+	public static void main(String[] args) {
+		ArrayList<Point> tabPoint = new ArrayList<Point>();
+		Point unpoint = new Point(100,100);
+		Fourmi unefourmi = new Fourmi(0, unpoint);
+		Ouvrier unOuvrier = new Ouvrier(unefourmi);
+		 tabPoint.add(unOuvrier.sedeplacer(unpoint));
+		 tabPoint.add(unOuvrier.sedeplacer(tabPoint.get(tabPoint.size()-1)));
+		 tabPoint.add(unOuvrier.sedeplacer(tabPoint.get(tabPoint.size()-1)));
+		 
+		 unOuvrier.rentreer = true;
+		 
+		 if(tabPoint.get(tabPoint.size()-1).equals(unOuvrier.sedeplacer(tabPoint.get(tabPoint.size()-1)))) {
+			 System.out.println("OK");
+		 }
+		 tabPoint.remove(tabPoint.size()-1);
+		 if(tabPoint.get(tabPoint.size()-1).equals(unOuvrier.sedeplacer(tabPoint.get(tabPoint.size()-1)))) {
+			 System.out.println("OK");
+		 }
+		 tabPoint.remove(tabPoint.size()-1);
+		 if(tabPoint.get(tabPoint.size()-1).equals(unOuvrier.sedeplacer(tabPoint.get(tabPoint.size()-1)))) {
+			 System.out.println("OK");
+		 }
+		
+	}
+
+}
