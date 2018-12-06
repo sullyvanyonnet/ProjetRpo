887968a6799f101ffa2e718642d65a11baf7ca73
yonnetsu <yonnetsu@localhost>
2018-12-06 16:44:38 +0100

    se deplacer ouvrier

--- src/Role/Ouvrier.java
+++ src/Role/Ouvrier.java
@@ -1,12 +1,41 @@
 package Role;
-
 import Fourmi.Fourmi;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Ouvrier extends Role{
+	
+	Boolean rentreer = false;
+	Stack<Point> PilePoint = new Stack<Point>(); 
 	
 	public Ouvrier(Fourmi fourmi) {
 	   	super(fourmi);
 	}
+	
+	Point sedeplacer(Point unPoint){
+	    Point retourCoordonnees = unPoint;
+		if(!rentreer) {	
+		    double direction = Math.random();	    
+		    if(direction < 0.25) {
+		    	retourCoordonnees.setLocation(unPoint.getX() - 1, unPoint.getY());
+		    } else if (direction < 0.50) {
+		    	retourCoordonnees.setLocation(unPoint.getX() + 1, unPoint.getY());
+		    } else if (direction < 0.75) {
+		    	retourCoordonnees.setLocation(unPoint.getX() ,unPoint.getY() + 1);
+		    } else if (direction < 1.00) {
+		    	retourCoordonnees.setLocation(unPoint.getX() ,unPoint.getY() - 1);
+		    }
+		    this.PilePoint.add(retourCoordonnees);	    
+		}else {
+			if(! this.PilePoint.empty()) {
+				retourCoordonnees =  this.PilePoint.pop();
+			}
+		}
+		return retourCoordonnees;
+	}
+	
+	
 	@Override
     public String monRole()
 	{
