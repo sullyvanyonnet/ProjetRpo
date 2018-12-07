478294a6953adebc6751d14e188741755c9072fb
yonnetsu <yonnetsu@localhost>
2018-12-07 15:09:16 +0100

    voila voila

--- src/proie/ProieLarve.java
+++ src/proie/ProieLarve.java
@@ -10,11 +10,13 @@ public class ProieLarve extends Proie {
 		//nbDeFourmiAttaquantes
 		//nbDeFourmiAttaquantesMax
 		//boolean mort
-
+	
+	//contructeur Larve avec 3 parametre
 	public ProieLarve(int identifiant, Terrain terrainLie) {
 		super(identifiant, PoidsEnMilligrammeLarve, terrainLie);
 	}
 	
+	//constructeur insecte avec 2 parametre et un poid par defaut	
 	public ProieLarve(int identifiant, int poids, Terrain terrainLie) {
 		super(identifiant, poids, terrainLie);
 	}
